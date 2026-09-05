d03ca0d6195ad499be096d761d045a08896258cb
skcreator470-droid <skcreator470@gmail.com>
2026-09-05 16:18:47 +0530

    Configure GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-
+  base: "/ner-smart-logistics/",
   server: {
     host: "127.0.0.1",
     port: 5173,
