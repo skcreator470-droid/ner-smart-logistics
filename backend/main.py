12b8d1f7b7090e17c5f86efe5cf3c8d97f84dd45
skcreator470-droid <skcreator470@gmail.com>
2026-09-05 16:38:46 +0530

    Allow GitHub Pages frontend

--- backend/main.py
+++ backend/main.py
@@ -76,6 +76,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
+    "https://skcreator470-droid.github.io",
     "http://localhost:5173",
     "http://127.0.0.1:5173",
     "http://localhost:5174",
